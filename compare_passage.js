79c9437f8317c3ef674e068584b0332884399757
Simon Miner <simon.miner@gmail.com>
2020-05-07 17:25:51 -0400

    Begin writing getBibleTranslationData() function (not yet working)

--- compare_passage.js
+++ compare_passage.js
@@ -2,15 +2,32 @@
 "use strict";
 
 /********************* Library Imports and Global Constants Setup ***********************/
+
 const fs = require("fs");
 const path = require("path");
 const util = require('util');
 const getopts = require("getopts")
 require('dotenv-safe').config();
 const exec = util.promisify(require("child_process").exec);
+const https = require("https");
 
 const options = getCommandLineOptions();
 var logger = getLogger();
+
+/********************************** Main Program *************************************/
+
+logger.info( `${path.basename( process.argv[1] )} starting run...` );
+
+var translations = undefined;
+( async () => {
+    try {
+        translations = await getBibleTranslationData();
+        logger.info( "translations = " + JSON.stringify( translations ) );
+    }
+    catch ( err ) {
+        logger.error( `${path.basename( process.argv[1] )} aborting with error: ${err}` );
+    }
+})();
 
 /********************************** Functions *************************************/
 
@@ -56,7 +73,7 @@ function getLogger() {
         message,
         timestamp
     }) => {
-        return `${timestamp} [${level} - ${message}`;
+        return `${timestamp} [${level}] - ${message}`;
     });
 
     const logDirectory = "./logs";
@@ -83,4 +100,34 @@ function getLogger() {
     });
 
     return logger;
+}
+
+/*
+ * const translations = getBibleTranslationData();
+ *
+ * Retrieve Bible translation data as a dictionary
+ * mapping translation name or abbreviation to id.
+ */
+async function getBibleTranslationData() {
+
+    const url = process.env.API_BASE_URL + "/bibles";
+    const options = {
+        method: 'GET',
+        hostname: process.env.API_HOST,
+        port: process.env.API_PORT,
+        path: process.env.API_BASE_PATH + "/bibles",
+
+        headers: {
+            accept: '*/*',
+            "api-key": process.env.API_KEY
+        }
+    };
+
+
+    var translations;
+    await https.request( options, res => {
+        console.log( JSON.stringify(res) ); 
+    });
+    
+    return translations;
 }
