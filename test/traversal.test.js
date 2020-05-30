5c9909877babd5a1e38a14286294e27e10a2665c
Simon Miner <simon.miner@gmail.com>
2020-05-30 17:56:22 -0400

    Add apiBaseURL and apiKey attributes to Tranversal class

--- test/traversal.test.js
+++ test/traversal.test.js
@@ -5,13 +5,20 @@
  *
  */
 
+const apiKey = 'test';
+const apiBaseURL = 'https://api.scripture.api.bible/v1';
+const language = 'English';
+const translation = 'KJV';
+
 const Traversal = require( '../lib/traversal' );
 
 test( 'constructor and default field values', () => {
-    const traversal = new Traversal();
+    const traversal = new Traversal( apiKey );
     expect( traversal instanceof Traversal ).toBe( true );
-    expect( traversal.language ).toBe( 'English' );
+    expect( traversal.language ).toBe( language );
     expect( Array.isArray( traversal.translations ) ).toBe( true );
     expect( traversal.translations.length ).toBe( 1 );
-    expect( traversal.translations[0] ).toBe( 'KJV' );
+    expect( traversal.translations[0] ).toBe( translation );
+    expect( traversal.apiKey ).toBe( apiKey );
+    expect( traversal.apiBaseURL ).toBe( apiBaseURL );
 });
